e8f9548e381abdbd3a30ba69f861640944508353
patraden <denis.patrakhin@gmail.com>
2020-11-22 22:13:11 +0000

    new

--- udacity_queue.py
+++ udacity_queue.py
@@ -1,8 +1,28 @@
-"""Make a Queue class using a list!
+"""
+Make a Queue class using a list!
 Hint: You can use any Python list method
 you'd like! Try to write each one in as 
 few lines as possible.
-Make sure you pass the test cases too!"""
+Make sure you pass the test cases too!
+
+>>> q = Queue(1)
+>>> q.enqueue(2)
+>>> q.enqueue(3)
+>>> print (q.peek())
+1
+>>> print (q.dequeue())
+1
+>>> q.enqueue(4)
+>>> print (q.dequeue())
+2
+>>> print (q.dequeue())
+3
+>>> print (q.dequeue())
+4
+>>> q.enqueue(5)
+>>> print (q.peek())
+5
+"""
 
 class Queue:
     def __init__(self, head=None):
@@ -16,28 +36,7 @@ class Queue:
 
     def dequeue(self):
         return self.storage.pop(0)
-    
-# Setup
-q = Queue(1)
-q.enqueue(2)
-q.enqueue(3)
 
-# Test peek
-# Should be 1
-print (q.peek())
-
-# Test dequeue
-# Should be 1
-print (q.dequeue())
-
-# Test enqueue
-q.enqueue(4)
-# Should be 2
-print (q.dequeue())
-# Should be 3
-print (q.dequeue())
-# Should be 4
-print (q.dequeue())
-q.enqueue(5)
-# Should be 5
-print (q.peek())
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
