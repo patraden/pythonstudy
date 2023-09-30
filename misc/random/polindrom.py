d5c53a33bfc89f01398b80a910e661e11480b444
d.patrakhin <d.patrakhin@propellerads.net>
2023-09-30 12:01:42 +0200

    random code

---
+++ misc/random/polindrom.py
@@ -0,0 +1,12 @@
+def solution(s):
+    l, r = 0, len(s) - 1
+    while l < r:
+        if s[l] != s[r]:
+            return False
+        l += 1
+        r -= 1
+    return True
+
+
+if __name__ == "__main__":
+    print(solution('aba'))
